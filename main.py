3db0946103835d80eb452d034f3ecfb4661b98b1
Iago Núñez Lourés <nunezlouresiago@gmail.com>
2024-06-14 01:03:23 +0200

    Actualización de checkpoints para la etl
    Solucionados conflictos entre parquets Añadida revisión para las funciones main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from ingesta.staging import staging
 from ingesta.business import business
 
 TEMP_DIR = './tmp'
-DOWNLOADED_DIR = "./archivos/downloaded"
+DOWNLOADED_DIR = "./archivos/downloaded/01-12"
 RAW_DIR = './archivos/raw'
 STAGING_DIR = './archivos/staging'
 BUSINESS_DIR = './archivos/business'
@@ -23,17 +23,17 @@ PORTS_DICTIONARY = {
     "dns": [5353]                       # 10
 }
 
-def main_raw(spark, raw_dir, temp_dir, file_writer, file_name='raw'):
+def main_raw(spark, raw_dir, temp_dir, downloaded_dir, file_writer, file_name='raw'):
 
-    raw_paths = FileSystemHandler.scan_directory('./archivos/downloaded', 'csv')
+    raw_paths = FileSystemHandler.scan_directory(downloaded_dir, 'csv')
 
     df = raw.merge_csv_files(spark, raw_paths)
     df = raw.stratify_dataframe(df)
     df = raw.refactor_headers(df)
 
-    raw_parquet_dir = f'{raw_dir}/{file_name}.parquet'
+    raw_parquet_dir = f'{raw_dir}/{file_name}'
 
-    file_writer.save_file(df, temp_dir, raw_parquet_dir)
+    df.repartition(1).write.format('parquet').mode('overwrite').save(raw_parquet_dir)
 
     return df
 
@@ -46,9 +46,9 @@ def main_staging(df, staging_dir, temp_dir, file_writer, file_name='staging'):
     df = staging.fill_with_max(df, "Flow_Packets/s")
     df = staging.fill_with_max(df, "Flow_Bytes/s")
 
-    staging_parquet_dir = f'{staging_dir}/{file_name}.parquet'
+    staging_parquet_dir = f'{staging_dir}/{file_name}'
 
-    file_writer.save_file(df, temp_dir, staging_parquet_dir)
+    df.repartition(1).write.format('parquet').mode('overwrite').save(staging_parquet_dir)
 
     return df
 
@@ -65,9 +65,9 @@ def main_bussiness (df, temp_dir, business_dir, ports_dict, file_writer, file_na
     df = business.ports_to_id(df, "Source_Port", ports_dict)
     df = business.ports_to_id(df, "Destination_Port", ports_dict)
 
-    staging_parquet_dir = f'{business_dir}/{file_name}.parquet'
+    staging_parquet_dir = f'{business_dir}/{file_name}'
 
-    file_writer.save_file(df, temp_dir, staging_parquet_dir)
+    df.repartition(1).write.format('parquet').mode('overwrite').save(staging_parquet_dir)
 
     return df
     
@@ -85,18 +85,18 @@ file_writer = SavePartitions(spark)
 #                 C A P A    R A W                 #
 ####################################################
 
-df = main_raw(spark, RAW_DIR, TEMP_DIR, file_writer)
+#df = main_raw(spark, RAW_DIR, TEMP_DIR, DOWNLOADED_DIR, file_writer)
 
 ######################################################
 #              C A P A    S T A G I N G              #
 ######################################################
 
-df = main_staging(df, STAGING_DIR, TEMP_DIR, file_writer)
+#df = main_staging(df, STAGING_DIR, TEMP_DIR, file_writer)
 
 ########################################################
 #              C A P A    B U S I N E S S              #
 ########################################################
-
+df = spark.read.parquet('./archivos/staging/staging/part-00000-cc401985-5f4a-4c87-a5ea-affb66f9c91c-c000.snappy.parquet', header=True, inferSchema=True)
 df = main_bussiness(df, BUSINESS_DIR, TEMP_DIR, PORTS_DICTIONARY, file_writer)
 
 SparkSessionHandler.stop_session(spark)
