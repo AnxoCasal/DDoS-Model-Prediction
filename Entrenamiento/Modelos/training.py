c7d8546c4b96eb29e824921bb33191ff0130f17a
AnxoCasal <anxocasal2002@gmail.com>
2024-06-17 16:10:03 +0200

    Retoques

--- Entrenamiento/Modelos/training.py
+++ Entrenamiento/Modelos/training.py
@@ -4,11 +4,15 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.model_selection import train_test_split
 
 class DataPreprocessing():
-    """
-    """
     
     @staticmethod
     def stratified_split_dataframe(df, target):
+        """
+        Divide el dataset de forma estratificada segun la columna objetivo
+        
+        :df: Dataframe
+        :target: columna objetivo sobre la que hacer la división
+        """
         
         X = df.drop(target, axis=1)
         y = df[target]
@@ -22,6 +26,13 @@ class DataPreprocessing():
     
     @staticmethod
     def apply_scaler(df, categorical_cols, scaler=None):
+        """
+        Aplica el MinMaxScaler a todas las columnas del dataset, ingorando unicamente las indicadas como categoricas. Además se puede pasar un scaler ya creado para utilizarlo
+
+        :df: Dataframe
+        :categorical_cols: Columnas marcadas como categóricas que serán ignoradas por el scaler
+        :scaler: Objeto scaler que se usara si existe, y en caso de que no se creará uno nuevo
+        """
 
         if scaler is None:
             scaler = MinMaxScaler()
@@ -33,6 +44,13 @@ class DataPreprocessing():
 
     @staticmethod
     def apply_lda(df, target, n_components):
+        """
+        Aplica la reduccion de dimecionalidad al dataset en base a una columna objetivo. Reduce las columnas que no séan objetivo en base al número que se le indique
+        
+        :df: Dataframe
+        :target: Columna objetivo por la que se va a guiar para reducir el resto
+        :n_components: Numero de columnas a la que se quiere reducir
+        """
         
         X = df.drop(target, axis=1)
         y = df[target]
@@ -48,6 +66,9 @@ class DataPreprocessing():
 
     @staticmethod
     def lda_variance_ratio(X, y):
+        """
+        Comprueba como varía la información según la cantidad de columnas a las que se reduzca usando el algoritmo LDA
+        """
         variance_ratio = {}
 
         n_features = X.shape[1]
@@ -59,3 +80,5 @@ class DataPreprocessing():
             lda.fit(X, y)
             explained_variance = lda.explained_variance_ratio_
             variance_ratio[i] = explained_variance.sum()
+
+        return variance_ratio
